61aae0ec351b3395c1f9c9c075fc647a8a9b9511
Daniel Main <dragma@gmail.com>
2026-01-26 12:52:03 -0700

    set up readding a set from the card to get images reloaded if needed.

--- reinstructible.client/src/legoset_owned/setCards/setCard/setCard.ts
+++ reinstructible.client/src/legoset_owned/setCards/setCard/setCard.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -14,9 +15,47 @@ import { ILegoSet } from '../../../interfaces/rebrickable'
 export class SetCard {
   public legoSet = input<ILegoSet | null>(null);
   public setNumOut = output<string>();
+  constructor(private http: HttpClient) { }
 
   loadSet(value: string) {
+    console.log("loadSet Clicked");
     this.setNumOut.emit(value);
   }
+
+  addSet(value: ILegoSet) {
+    console.log("add set clicked");
+    event?.stopPropagation();
+    this.saveSet(value);
+  }
+  saveSet(legoSet: ILegoSet) {
+    let result = {};
+    let loading = true;
+    let error = "";
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'response' as 'response', // To get the full HttpResponse
+    };
+
+    // const body = { 'legoSetJSON' : JSON.stringify(legoSet) };
+    const data: string = legoSet.set_num;
+    const jsonString = JSON.stringify(data);
+
+
+    this.http.post('/api/set', jsonString, httpOptions).subscribe({
+      next: (res) => {
+        result = res;
+        loading = false;
+        //this.addSetEvent.emit(legoSet);
+      },
+      error: (error) => {
+        console.error(error);
+        error = 'Failed to create post'; loading = false;
+      }
+    });
+  }
+
 }
 
