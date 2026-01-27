e86036037e63cb764e1a1fb2a7ea4e837f9d0654
Daniel Main <dragma@gmail.com>
2026-01-27 13:52:49 -0700

    Adding set reloading to the table view

--- reinstructible.client/src/legoset_owned/setTable/setTable.ts
+++ reinstructible.client/src/legoset_owned/setTable/setTable.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit, OnChanges, signal, input, SimpleChanges, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +17,43 @@ export class SetTable {
   public legoSets = input<ILegoSet[]>([]);
   public setNumOut = output<string>();
 
+  constructor(private http: HttpClient) { }
+
   loadSet(value: string) {
     this.setNumOut.emit(value);
+  }
+  addSet(value: ILegoSet) {
+    console.log("add set clicked");
+    event?.stopPropagation();
+    this.saveSet(value);
+  }
+  saveSet(legoSet: ILegoSet) {
+    let result = {};
+    let loading = true;
+    let error = "";
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'response' as 'response', // To get the full HttpResponse
+    };
+
+    // const body = { 'legoSetJSON' : JSON.stringify(legoSet) };
+    const data: string = legoSet.set_num;
+    const jsonString = JSON.stringify(data);
+
+
+    this.http.post('/api/set', jsonString, httpOptions).subscribe({
+      next: (res) => {
+        result = res;
+        loading = false;
+        //this.addSetEvent.emit(legoSet);
+      },
+      error: (error) => {
+        console.error(error);
+        error = 'Failed to create post'; loading = false;
+      }
+    });
   }
 }
